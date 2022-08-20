5f4d9cd56da21fcfd58d59407cd975ff55c57cd6
Andres <andresch0702@gmail.com>
2022-08-20 13:19:28 -0600

    Create Teclado.sv

---
+++ Source/Design/Teclado.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.08.2022 13:18:38
+// Design Name: 
+// Module Name: Teclado
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Teclado(
+
+    );
+endmodule
