5a1f88a741473e33c8a2b8328dfdbf032caeb5f9
AndreyMZ20 <110086033+AndreyMZ20@users.noreply.github.com>
2022-09-06 09:52:00 -0600

    Ejercicio_7_tecla_debounce

--- Ejercicios/Proyectos/Ejercicio7/Ej7/vivado_project.runs/synth_1/runme.sh
+++ Ejercicios/Proyectos/Ejercicio7/Ej7/vivado_project.runs/synth_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/carlo/Andrey/TEC/Semestre_II_2022/Taller_Digitales/Laboratorios/Laboratorio_2/Repositorio/lab02-g03/Ejercicios/Proyectos/Ejercicio7/vivado_project.runs/synth_1'
+HD_PWD='C:/Users/carlo/Andrey/TEC/Semestre_II_2022/Taller_Digitales/Laboratorios/Laboratorio_2/Repositorio/lab02-g03/Ejercicios/Proyectos/Ejercicio7/Ej7/vivado_project.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
